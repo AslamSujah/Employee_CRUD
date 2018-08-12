a735a816ec94678abc12b58bb9d5961f28e3bc73
sujaslam <ameersujah@yahoo.com>
2018-08-12 08:37:37 +0530

    Develop the interface for basic CRUD operations.

---
+++ Employee_Management_System/Core.Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data
+{
+    public interface IRepository : IDisposable
+    {
+        IEnumerable<T> GetAll<T>() where T : class;
+
+        IEnumerable<T> GetById<T>(int id) where T : class;
+
+        IEnumerable<T> GetByName<T>(string name) where T : class;
+
+        void Add<T>(T entity) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+        void Delete<T>(T entity) where T : class;
+    }
+}
